ca40172ddb69e3759fb36b4fea7447b868829458
Sospeter <sosmogendi@gmail.com>
2024-01-28 11:55:52 +0000

    Updated my pagination function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -6,6 +6,10 @@
 def index_range(page, page_size):
     '''
         Returns the range of indexes for a given page.
+
+        Parameters:
+    - page (int): The current page number (1-indexed).
+    - page_size (int): The number of items per page.
     '''
     start_index = (page - 1) * page_size
     end_index = page * page_size
