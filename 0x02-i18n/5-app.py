60646bd3c0dfe402c68a4477e93108d0903adc6d
Sospeter <sosmogendi@gmail.com>
2024-02-06 21:38:39 +0000

    Mock logging in

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 '''
-    User locale detection and login emulation with Flask and Babel.
+    Emulate user login and detect user locale with Flask and Babel.
 '''
 
 from flask import Flask, render_template, request, g
@@ -13,9 +13,9 @@ class Config(object):
     '''
         Babel configuration.
     '''
-    SUPPORTED_LANGUAGES = ['en', 'fr']
-    DEFAULT_LOCALE = 'en'
-    DEFAULT_TIMEZONE = 'UTC'
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 app.config.from_object(Config)
 
@@ -26,33 +26,26 @@ users = {
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
 
-def fetch_user() -> Union[dict, None]:
+def get_user() -> dict:
     '''
-        Fetch user information based on user ID provided in the URL parameter.
+        Fetch user information based on the user ID provided in the URL parameter.
     '''
-    try:
-        login_as = request.args.get('login_as', None)
-        if login_as:
-            return users.get(int(login_as))
-        else:
-            return None
-    except Exception:
-        return None
+    user_id = request.args.get('login_as')
+    return users.get(int(user_id)) if user_id and int(user_id) in users else None
 
 @app.before_request
 def before_request():
     '''
         Set the user as a global variable on flask.g.user.
     '''
-    user = fetch_user()
-    g.current_user = user if user else None
+    g.user = get_user()
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index():
     '''
         Render template with a welcome message based on the user's login status.
     '''
-    welcome_message = _('logged_in_as', username=g.current_user['name']) if g.current_user else _('not_logged_in')
+    welcome_message = _('logged_in_as', username=g.user['name']) if g.user else _('not_logged_in')
     return render_template('5-index.html', welcome_message=welcome_message)
 
 @babel_instance.localeselector
@@ -61,9 +54,9 @@ def get_locale() -> str:
         Get user locale from URL parameters or default to accept languages.
     '''
     locale = request.args.get('locale')
-    if locale in app.config['SUPPORTED_LANGUAGES']:
+    if locale in app.config['LANGUAGES']:
         return locale
-    return request.accept_languages.best_match(app.config['SUPPORTED_LANGUAGES'])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 if __name__ == '__main__':
     app.run()
