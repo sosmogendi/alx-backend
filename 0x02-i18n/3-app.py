b1a0b9de66bd66bfbd253e7929e63f33cf1f2af0
Sospeter <sosmogendi@gmail.com>
2024-02-06 21:06:34 +0000

    Parametrize templates

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+'''
+    Use Babel to get user locale.
+'''
+
+from flask import Flask, render_template
+from flask_babel import Babel, _
+
+app = Flask(__name__, template_folder='templates')
+babel_instance = Babel(app)
+
+class Config(object):
+    '''
+        Babel configuration.
+    '''
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+app.config.from_object(Config)
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def hello_world() -> str:
+    '''
+        Render template for Babel usage.
+    '''
+    return render_template('3-index.html')
+
+@babel_instance.localeselector
+def get_locale() -> str:
+    '''
+        Get user locale to serve matching translation.
+    '''
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+if __name__ == '__main__':
+    app.run()
