f9044f86022c70b1fbdac1f32fcf395e110f0b2d
Sospeter <sosmogendi@gmail.com>
2024-02-06 20:18:44 +0000

    Get locale from request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -14,16 +14,16 @@ class Config(object):
     '''
         Babel configuration.
     '''
-    SUPPORTED_LANGUAGES = ['en', 'fr']
-    DEFAULT_LOCALE = 'en'
-    DEFAULT_TIMEZONE = 'UTC'
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app.config.from_object(Config)
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
-def helloWorld() -> str:
+def hello_world() -> str:
     '''
         Render template for Babel usage.
     '''
@@ -35,7 +35,7 @@ def get_locale() -> str:
     '''
         Get user locale.
     '''
-    return request.accept_languages.best_match(app.config['SUPPORTED_LANGUAGES'])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 if __name__ == '__main__':
