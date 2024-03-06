274126c3b47a6a790a19990f82ae96a986cdc871
Sospeter <sosmogendi@gmail.com>
2024-03-06 20:39:14 +0000

    Job processor

---
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -0,0 +1,26 @@
+import kue from 'kue';
+
+const blacklistedNumbers = ['4153518780', '4153518781'];
+
+function sendNotification(phoneNumber, message, job, done) {
+  const total = 100;
+
+  job.progress(0, total);
+
+  if (blacklistedNumbers.includes(phoneNumber)) {
+    done(new Error(`Phone number ${phoneNumber} is blacklisted`));
+    return;
+  }
+
+  job.progress(50, total);
+  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+  done();
+}
+
+const queue = kue.createQueue();
+const queueName = 'push_notification_code_2';
+
+queue.process(queueName, 2, (job, done) => {
+  const { phoneNumber, message } = job.data;
+  sendNotification(phoneNumber, message, job, done);
+});
