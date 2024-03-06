230c26b0f81fada74e1d73b0bf0236806958b502
Sospeter <sosmogendi@gmail.com>
2024-03-06 20:47:17 +0000

    job creation function

---
+++ 0x03-queuing_system_in_js/8-job.js
@@ -0,0 +1,33 @@
+function createPushNotificationsJobs(jobs, queue) {
+  if (!Array.isArray(jobs)) {
+    throw new Error('Jobs is not an array');
+  }
+
+  const queueName = 'push_notification_code_3';
+
+  jobs.forEach((jobFormat) => {
+    const job = queue.create(queueName, jobFormat);
+
+    job.save((err) => {
+      if (err) {
+        console.error(`Failed to create notification job: ${err}`);
+      } else {
+        console.log(`Notification job created: ${job.id}`);
+      }
+    });
+
+    job.on('complete', () => {
+      console.log(`Notification job ${job.id} completed`);
+    });
+
+    job.on('failed', (errorMessage) => {
+      console.error(`Notification job ${job.id} failed: ${errorMessage}`);
+    });
+
+    job.on('progress', (progress) => {
+      console.log(`Notification job ${job.id} ${progress}% complete`);
+    });
+  });
+}
+
+export default createPushNotificationsJobs;
